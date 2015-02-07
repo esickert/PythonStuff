720956bca6eb01f803bc04e806e2c7f7a6ac97b4
Erich Sickert <esickert@gmail.com>
2015-02-06 21:57:13 -0800

    Added some stuff

--- PythonPlayground/bottlesOfBeer.py
+++ PythonPlayground/bottlesOfBeer.py
@@ -1,3 +1,10 @@
-for i in range(4,0,-1):
-    print str(i) + " bottles of beer on the wall, " + str(i -1) + " Bottles of beer"
-
+#for i in range(4,0,-1):
+#    print str(i) + " bottles of beer on the wall, " + str(i -1) + " Bottles of beer"
+i = 0
+while i < 4:
+    #print("heloo")
+    print (str(i) + " bottles of beer on the wall, " + str(i - 1) + " bottles of beer")
+    i = i + 1  
+#while i < 4:
+#    print ("Hello")
+#    i = i + 1
