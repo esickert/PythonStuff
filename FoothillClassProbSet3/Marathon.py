2b503335b5313cda2a0db4196d7536bede0cc17b
Erich Sickert <esickert@gmail.com>
2015-02-16 00:00:40 -0800

    Added Marathon.py

--- FoothillClassProbSet3/Marathon.py
+++ FoothillClassProbSet3/Marathon.py
@@ -8,7 +8,29 @@ times = [
 		341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299,
 		343, 317, 265
 		]
-print("Name:", "	","Time:")
-print("****************")
-for index in range(len(names)):
-	print (names[index], "	" ,times[index])
+print("Name:", "		","Time:")
+print("*************************")
+#***********************************************************************
+temp = times[0];
+for i in range(0,len(times)):
+	if (names[i] == "Hamilton"):
+		print(names[i],"	",times[i], " minutes");
+	else:
+		print (names[i], "		" ,times[i]," minutes");
+#***********************************************************************
+for i in range(0,len(times)):
+	if (times[i] < temp):
+		temp = times[i];
+		index = i;
+print("*************************");
+print(names[index], "has the best time of ", temp, " minutes");
+#***********************************************************************
+#use the index of the best time to figure out the time of the second best runner.
+temp = times[0];
+for i in range(0,len(times)):
+	if (i == index):
+		continue;
+	elif (times[i]  < temp):  
+		temp = times[i];
+print(names[i], "has the second best time of ", temp, " minutes");
+print();
