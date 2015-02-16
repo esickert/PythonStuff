2d483208aa862b2db489e5c5d65e29e69ee41f9a
Erich Sickert <esickert@gmail.com>
2015-02-15 19:45:16 -0800

    Added python script files from java class homework.

---
+++ FoothillClassProbSet3/Marathon.py
@@ -0,0 +1,14 @@
+names = [
+		"Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex",
+		"Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda",
+		"Aaron", "Kate"
+		]
+		
+times = [
+		341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299,
+		343, 317, 265
+		]
+print("Name:", "	","Time:")
+print("****************")
+for index in range(len(names)):
+	print (names[index], "	" ,times[index])
