5fd15a4393105b4249c88a8dd0d56c4740b9560b
esickert <esickert2@gmail.com>
2024-01-28 18:07:55 -0800

    Testing with python

--- PythonPlayground/PythonTicker.py
+++ PythonPlayground/PythonTicker.py
@@ -1,11 +1,13 @@
 import time
 
-print "SPAM SPAM SPAM SPAM SPAM SPAM SPAM"
-raw_input('Press the <ENTER> key to continue')
-for x in xrange(5, 11):
-  print "Monty Python's Flying Circus"
+print("SPAM SPAM SPAM SPAM SPAM SPAM SPAM")
+#this had python 2 errors, converted to python 3
+input('Press the <ENTER> key to continue')
+for x in \
+range(5, 11):  #xrange no longer used in python3
+  print ("Monty Python's Flying Circus")
   time.sleep(2) 
 time.sleep(5.5) 
-print "That's all folks!!"
+print ("That's all folks!!");
 
 
