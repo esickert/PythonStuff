5b78253a148fde2d9bc13bf5f1595fd82ed96743
esickert <esickert2@gmail.com>
2024-04-29 07:27:40 -0700

    This is a test for python coding

---
+++ PythonPlayground/Practice.py
@@ -0,0 +1,15 @@
+#print("This is a practice file");
+word = "aaaaa"
+back = "aaaa"[::-1]
+
+#languages = ['Swift', 'Python', 'Go']
+
+# access elements of the list one by one
+#for i in word:
+ #   print(i,end="")
+if word == back:
+    print("Palindome")
+else:
+    print("NOT A PALINDROME");
+
+print("************************************")
