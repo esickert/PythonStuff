b1b41f24eff9409d6244fee455037ecc0a8274b1
esickert <esickert2@gmail.com>
2024-04-29 11:00:01 -0700

    This is a test for python coding

--- PythonPlayground/Practice.py
+++ PythonPlayground/Practice.py
@@ -1,15 +1,17 @@
 #print("This is a practice file");
 word = "aaaaa"
-back = "aaaa"[::-1]
+back = "aaaaa"[::-1]
 
 #languages = ['Swift', 'Python', 'Go']
 
 # access elements of the list one by one
 #for i in word:
  #   print(i,end="")
+print("PALINDROME");
 if word == back:
     print("Palindome")
 else:
     print("NOT A PALINDROME");
 
 print("************************************")
+print("FIBINACCI SEQUENCE")
