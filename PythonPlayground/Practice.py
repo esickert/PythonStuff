394f0c4dadbc0c6018d734972e4bcbef27d6dc85
esickert <esickert2@gmail.com>
2024-05-21 20:44:28 -0700

    This is a test for python coding

--- PythonPlayground/Practice.py
+++ PythonPlayground/Practice.py
@@ -23,7 +23,7 @@ class TestingPython(unittest.TestCase):
             print(c, end=' ')
             a = b
             b = c
-#*************************************
+#*********************************
 
  #   def test_fibrec(self):
  #       a = 0;
