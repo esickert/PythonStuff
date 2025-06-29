b9868dccedc9fd02605c994341b24ec3881f9ac4
esickert <esickert2@gmail.com>
2025-06-28 20:43:54 -0700

    This is a test for python coding

--- PythonPlayground/Practice.py
+++ PythonPlayground/Practice.py
@@ -3,18 +3,25 @@ class TestingPython(unittest.TestCase):
 #print("This is a practice file");
 
     def test_palindrome(self):
+      #  n + "TEST""
         word = "aaa"
-        back = "aaa"
+        back = "aa"
         if word == back:
-            print("\nThis is a Palindrome")
+            print("\nThis is a Palindrome");
             print("line 10")
         else:
             print("\nThis is NOT A PALINDROME")
-            print("line 13", n);
+            print("This is so FUCKED!!!!")
+           #   test_palindrome;
 """
+   
+
+
+
 
             #!/usr/bin/env python
-
+for x in range(3):
+  print(x)
 
 #fibonacci_recursive(10)
 """
